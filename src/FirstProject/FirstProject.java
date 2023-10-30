124fed00b1afb43ccef5f62c9eb47ec8b7a414aa
Janak singh dhami <info@jsdhami.com.np>
2023-10-30 15:39:52 +0545

    Add package

---
+++ src/FirstProject/FirstProject.java
@@ -0,0 +1,17 @@
+package FirstProject;
+
+public class FirstProject{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        String name = "Janak";
+        System.out.println("My name is " + name);
+        int age = 20;
+        System.out.println("I am " + age + " years old");
+        // This is a comment
+
+        /* This is a multi-line comment */
+
+
+
+    }
+}
